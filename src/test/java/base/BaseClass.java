df29e6afa877be103420b36a9963fc24fbcb260a
bahodurotayev <tjkbaha@gmail.com>
2023-03-06 18:48:28 -0500

    CucumberProject

---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,67 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import utils.CommonMethods;
+import utils.ConfigsReader;
+import utils.Constants;
+
+import java.time.Duration;
+
+// NOTE: THIS CLASS IS USED TO LAUNCH AND QUIT THE BROWSER
+
+public class BaseClass extends CommonMethods {
+    public static WebDriver driver;
+    public static void setUp() {
+        ConfigsReader.loadProperties(Constants.CONFIGURATION_FILEPATH); // Replaced hard-coded filePath with Constants
+        String headless = ConfigsReader.getProperties("headless");
+        switch (ConfigsReader.getProperties("browser").toLowerCase()) {
+            case "chrome" -> {
+                //System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
+                WebDriverManager.chromedriver().setup();
+                if (headless.equalsIgnoreCase("true")){
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--headless", "--log-level=3");
+                    options.addArguments("--disable-logging");
+                    driver = new ChromeDriver(options);
+                }else {
+                    driver = new ChromeDriver();
+                }
+            }
+            case "edge" -> {
+                //System.setProperty("webdriver.edge.verboseLogging", Constants.EDGE_DRIVER_PATH);
+                WebDriverManager.edgedriver().setup();
+                if (headless.equalsIgnoreCase("true")){
+                    EdgeOptions options = new EdgeOptions();
+                    options.addArguments("--headless");
+                    options.addArguments("--disable-logging");
+                    driver = new EdgeDriver(options);
+                }else {
+                    driver = new EdgeDriver();
+                }
+            }
+            default -> throw new RuntimeException("Browser is not supported");
+        }
+
+        driver.get(ConfigsReader.getProperties("url"));
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Constants.IMPLICIT_WAIT_TIME));
+        initialize();
+    }
+
+    public static void tearDown() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.getStackTrace();
+        }
+        if (driver != null) {     // This line is optional. We only use it to prevent NullPointerException.
+        driver.quit();
+        }
+    }
+
+}
---
+++ src/test/java/utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+    public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir") + "/src/test/resources/configs/configuration.properties";
+    public static final String TEST_DATA_EXCEL = System.getProperty("user.dir") + "/src/test/resources/testdata/ExelenterEmployeesList.xlsx";
+    public static final int IMPLICIT_WAIT_TIME = 15;
+    public static final int EXPLICIT_WAIT_TIME = 40;
+
+}
